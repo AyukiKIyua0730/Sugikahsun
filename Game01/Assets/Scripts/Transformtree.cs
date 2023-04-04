cf82678a00097fdde137013e84bb5e44cf5d7f4a
2122075-PC\2122075 <s2122075@cco.kanagawa-it.ac.jp>
2023-04-05 02:10:51 +0900

    木の挙動途中まで、シーン作成終了

---
+++ Game01/Assets/Scripts/Transformtree.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformtree : MonoBehaviour
+{
+
+    public float radius = 10f;
+
+    Vector3 initPos;
+    float speed = 0.5f;
+    Vector2 pos;
+    bool push = false;
+    private float totalTime = 0f;
+    void Start()
+    {
+
+        
+        float phase = speed * Mathf.PI;
+
+        float xPos = radius * Mathf.Cos(phase);
+        float yPos = radius * Mathf.Sin(phase);
+
+        pos = new Vector2(xPos, yPos);
+        gameObject.transform.position = pos;
+
+        initPos = gameObject.transform.position;
+    }
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            push = true;
+            speed += 0.001f;
+            
+            float phase = speed*totalTime * Mathf.PI;
+            
+            float xPos = radius * Mathf.Cos(phase);
+            float yPos = radius * Mathf.Sin(phase);
+
+            pos = new Vector2(xPos, yPos);
+            gameObject.transform.position = pos;
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            push = true;
+            speed -= 0.001f;
+
+            float phase = speed * totalTime * Mathf.PI;
+            float xPos = radius * Mathf.Cos(phase);
+            float yPos = radius * Mathf.Sin(phase);
+      
+            pos = new Vector2(xPos, yPos);
+            gameObject.transform.position = pos;
+        }
+        else
+        {
+            push = false;
+        }
+        if (push == false)
+        {
+            totalTime *= 0.1f;
+            if (initPos.x < pos.x)
+            {
+               
+                speed += 0.000333f;
+
+                float phase = speed * totalTime * Mathf.PI;
+
+                float xPos = radius * Mathf.Cos(phase);
+                float yPos = radius * Mathf.Sin(phase);
+
+                pos = new Vector2(xPos, yPos);
+                gameObject.transform.position = pos;
+            }
+            else if (initPos.x > pos.x)
+            {
+                speed -= 0.000333f;
+
+                float phase = speed * totalTime * Mathf.PI;
+
+                float xPos = radius * Mathf.Cos(phase);
+                float yPos = radius * Mathf.Sin(phase);
+
+                pos = new Vector2(xPos, yPos);
+                gameObject.transform.position = pos;
+            }
+        }
+
+
+        
+        
+    }
+    /// <Summary>
+    /// オブジェクトの位置を計算するメソッドです。
+    /// </Summary>
+    
+}
