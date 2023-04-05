f9c0f5681d6a524b3be2d5ad74a732f2ab856174
2122075-PC\2122075 <s2122075@cco.kanagawa-it.ac.jp>
2023-04-05 10:55:40 +0900

    カウンター仮置き

---
+++ Game01/Assets/Scripts/Counter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public GameObject score_object; // Textオブジェクト
+    public int score_num = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text score_text = score_object.GetComponent<Text>();
+        score_text.text = "Score:" + score_num;
+    }
+}
