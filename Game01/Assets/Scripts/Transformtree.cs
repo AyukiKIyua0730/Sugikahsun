4561c87fc518c69755057afd2e4d2fb99f3d3587
2122075-PC\2122075 <s2122075@cco.kanagawa-it.ac.jp>
2023-04-06 01:18:41 +0900

    木の物理振り子

--- Game01/Assets/Scripts/Transformtree.cs
+++ Game01/Assets/Scripts/Transformtree.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class Transformtree : MonoBehaviour
 {
+    /*private Rigidbody rb;
+    float speed = 1.0f;
+    float radius = 3.0f;
 
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        rb.MovePosition(
+            new Vector2(
+                radius * Mathf.Sin(Time.time * speed),
+                radius * Mathf.Cos(Time.time * speed)
+             )
+        );
+    }*/
     public float radius = 10f;
 
     Vector3 initPos;
-    float speed =0.5f;
+    float speed =0f;
     Vector2 pos;
     bool push = false;
     private float totalTime = 0f;
+    private float Addforce = 0f;
+    [SerializeField] GameObject target;
     void Start()
     {
 
         
-        float phase = speed * Mathf.PI;
+        float phase = 0.5f * Mathf.PI;
 
         float xPos = radius * Mathf.Cos(phase);
         float yPos = radius * Mathf.Sin(phase);
@@ -32,60 +51,51 @@ public class Transformtree : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             push = true;
-            speed += 0.001f;
+            Addforce += 0.00001f;
+            Addforce += Time.deltaTime;
             
-            float phase = speed* Time.deltaTime * Mathf.PI;
-            
-            float xPos = radius * Mathf.Cos(phase);
-            float yPos = radius * Mathf.Sin(phase);
-
-            pos = new Vector2(xPos, yPos);
-            gameObject.transform.position = pos;
-
+            gameObject.transform.RotateAround(target.transform.position, Vector3.forward, 10f * Time.deltaTime*Addforce);
+            pos = gameObject.transform.position;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             push = true;
-            speed -= 0.001f;
-
-            float phase = speed * Time.deltaTime * Mathf.PI;
-            float xPos = radius * Mathf.Cos(phase);
-            float yPos = radius * Mathf.Sin(phase);
-      
-            pos = new Vector2(xPos, yPos);
-            gameObject.transform.position = pos;
+            Addforce += 0.00001f;
+            Addforce += Time.deltaTime;
+          
+            gameObject.transform.RotateAround(target.transform.position, Vector3.forward, -10f * Time.deltaTime * Addforce);
+            pos = gameObject.transform.position;
         }
         else
         {
             push = false;
         }
+        
+        
         if (push == false)
         {
-            totalTime = 0f;
+            
             if (initPos.x < pos.x)
             {
                 Debug.Log("A");
-                speed += 0.000333f;
-               
-                float phase = speed  * Mathf.PI;
+                
+              
+                Addforce += Time.deltaTime;
 
-                float xPos = radius * Mathf.Cos(phase);
-                float yPos = radius * Mathf.Sin(phase);
-
-                pos = new Vector2(xPos, yPos);
-                gameObject.transform.position = pos;
+                // gameObject.transform.position = pos;
+                gameObject.transform.RotateAround(target.transform.position, Vector3.forward, 10f * Time.deltaTime * Addforce);
+                pos = gameObject.transform.position;
+                push = false;
             }
             else if (initPos.x > pos.x)
             {
-                speed -= 0.000333f;
-               
-                float phase = speed  * Mathf.PI;
+                Debug.Log("B");
+                
+                Addforce -= Time.deltaTime;
 
-                float xPos = radius * Mathf.Cos(phase);
-                float yPos = radius * Mathf.Sin(phase);
-
-                pos = new Vector2(xPos, yPos);
-                gameObject.transform.position = pos;
+                gameObject.transform.RotateAround(target.transform.position, Vector3.forward, -10f * Time.deltaTime * Addforce);
+                pos = gameObject.transform.position;
+                push = false;
             }
         }
 
@@ -96,5 +106,61 @@ public class Transformtree : MonoBehaviour
     /// <Summary>
     /// オブジェクトの位置を計算するメソッドです。
     /// </Summary>
+    /// 
     
+    /*
+    private float moveSpeed = 0.01f;
+
+    private float circle_radius = 3.0f;
+
+    private Vector2 initPosition;
+
+    private float Addfoce;
+    void Start()
+    {
+        Vector2 pos = transform.position;
+        float rad = 1.0f * Mathf.Rad2Deg;
+
+        pos.x = Mathf.Cos(rad) * circle_radius;
+
+        pos.y = Mathf.Sin(rad) * circle_radius;
+        transform.position = pos;
+        initPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Addfoce += Time.deltaTime;
+            Vector2 pos = transform.position;
+            moveSpeed += 0.00001f;
+            float rad = moveSpeed * Mathf.Rad2Deg * Addfoce;
+
+            pos.x = Mathf.Cos(rad) * circle_radius;
+
+            pos.y = Mathf.Sin(rad) * circle_radius;
+            transform.position = pos;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Addfoce += Time.deltaTime;
+            Vector2 pos = transform.position;
+            moveSpeed -= 0.00001f;
+            float rad = moveSpeed * Mathf.Rad2Deg * Addfoce;
+
+            pos.x = Mathf.Cos(rad) * circle_radius;
+
+            pos.y = Mathf.Sin(rad) * circle_radius;
+
+            transform.position = pos;
+        }
+        else
+        {
+            
+        }
+        
+
+        
+    }*/
 }
